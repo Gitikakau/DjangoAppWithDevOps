a51cf4737a5bf9defc3ac187621c83c214e65ab7
Gitika <146334680+Gitikakau@users.noreply.github.com>
2025-08-20 02:44:04 +0530

    Update settings.py

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -74,7 +74,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'STATIC')]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
